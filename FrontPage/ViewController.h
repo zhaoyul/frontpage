480a200afbd663ebb27eee16c3c440a87cc18be5
Kevin.Li <li_zhaoyu@msn.com>
2015-05-29 08:23:26 +0800

    Initial Commit

---
+++ FrontPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FrontPage
+//
+//  Created by Zhaoyu Li on 5/29/15.
+//  Copyright (c) 2015 Zhaoyu Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
